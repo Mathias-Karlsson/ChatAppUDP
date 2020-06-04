ca091dab5760c01771945531ca38fef84a8398d2
mathkarl <mathkarl@LSE-B2725>
2020-06-04 11:26:17 +0100

    Add project files.

---
+++ ChatAppUDP/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ChatAppUDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serverIP = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbxMeddelande = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serverIP
+            // 
+            this.serverIP.Location = new System.Drawing.Point(132, 12);
+            this.serverIP.Name = "serverIP";
+            this.serverIP.Size = new System.Drawing.Size(107, 20);
+            this.serverIP.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // tbxMeddelande
+            // 
+            this.tbxMeddelande.Location = new System.Drawing.Point(12, 88);
+            this.tbxMeddelande.Multiline = true;
+            this.tbxMeddelande.Name = "tbxMeddelande";
+            this.tbxMeddelande.Size = new System.Drawing.Size(227, 72);
+            this.tbxMeddelande.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(12, 166);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(227, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Sänd";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Serverns IP-Adress";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Serverns Portnummer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Utkorg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 190);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbxMeddelande);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.serverIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox serverIP;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbxMeddelande;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ChatAppUDP/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatAppUDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            byte[] message = Encoding.Unicode.GetBytes(tbxMeddelande.Text);
+
+            IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+            IPEndPoint destination = new IPEndPoint(serverIP, 12345);
+
+            UdpClient klient = new UdpClient();
+            klient.Send(message, message.Length, destination);
+        }
+    }
+}
